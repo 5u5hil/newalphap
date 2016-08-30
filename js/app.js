d4b26bdec86969b9c6ecf84672fba46818053b56
Bhavana Nikhade <bhavana@infiniteit.biz>
2016-08-30 17:48:11 +0530

    xvzxxv

--- js/app.js
+++ js/app.js
@@ -212,7 +212,7 @@ angular.module('your_app_name', [
                     }, 0);
                 }
                 try {
-                    if (toState.name == "app.patient-join")
+                     if (toState.name == "app.doctor-join" || toState.name == "app.chat")
                     {
                         console.log("false state");
                         window.plugins.OneSignal.enableInAppAlertNotification(false);
@@ -223,6 +223,8 @@ angular.module('your_app_name', [
                 } catch (err) {
 
                 }
+                
+               
             });
             $rootScope.$on("$stateChangeSuccess", function (event, toState, toParams, fromState, fromParams) {
                 if (toState.name.indexOf('app.feeds-categories') > -1)
